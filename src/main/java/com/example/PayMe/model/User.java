9c7eb6fda6d2dcfc14b6240c98ba9ca7e0646a06
MaDesOcr <madesocr@gmail.com>
2024-05-23 16:28:43 +0200

    Couche model

--- src/main/java/com/example/PayMe/model/User.java
+++ src/main/java/com/example/PayMe/model/User.java
@@ -1,5 +1,35 @@
 package com.example.PayMe.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private float balance;
+	
+	private String iban;
+	
+	private String email;
+	
+	private String pwd;
+	
+	@ManyToMany
+	private List<User> listContacts;
+	
+	@OneToMany(mappedBy ="sender")
+	private List<Transaction> listeTransaction;
 }
--- src/main/java/com/example/PayMe/model/Virement.java
+++ src/main/java/com/example/PayMe/model/Virement.java
@@ -1,5 +1,26 @@
 package com.example.PayMe.model;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Virement {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	private User user;
+	
+	private Date date;
+	
+	private float amount;
+	
+	private String label;
 }
